0311002cba6f5342def825fac5a9479d2095bb3a
Nupuri Islam <88903452+nupuri-islam@users.noreply.github.com>
2021-09-12 23:54:43 +0600

    project-1

---
+++ Assignment-1/function.js
@@ -0,0 +1,5 @@
+function myFunction(){
+    document.getElementById("form-submit").innerHTML = "Successfully Submitted!";
+    document.getElementById("form-submit").style.color = "#059862";
+    document.getElementById("form-submit").style.fontSize = "25px";
+}
